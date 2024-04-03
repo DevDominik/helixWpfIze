1770254834af70f67f37871efdda067cd984fc34
DevDominik <71924117+DevDominik@users.noreply.github.com>
2024-04-03 14:40:20 +0200

    Add files via upload

---
+++ WpfApp1/Bolygo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+
+    public interface IBolygo
+    {
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+
+
+        public bool Szabad { get; set; }
+    }
+    public class Bolygo : IBolygo
+    {
+
+        public Bolygo(double x, double y, double z)
+        {
+            X = x; Y = y; Z = z;
+        }
+
+        public bool Szabad { get; set; }
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+
+        public static double Tavolsag(Bolygo egyik, Bolygo masik)
+        {
+            return Math.Sqrt(Math.Pow(masik.X - egyik.X, 2) + Math.Pow(masik.Y - egyik.Y, 2) + Math.Pow(masik.Z - egyik.Z, 2));
+        }
+
+        public List<Bolygo> VisszadobKoronBelul(List<Bolygo> bolygok)
+        {
+            List<Bolygo> vissza = new();
+            foreach (Bolygo bolygo in bolygok)
+            {
+                if (bolygo != this && Szabad == true && Tavolsag(bolygo, this) <= 900)
+                {
+                    vissza.Add(bolygo);
+                }
+            }
+            return vissza;
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using HelixToolkit.Wpf;
-using System.Drawing;
-using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,32 +14,100 @@ using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using WpfApp1;
 
-namespace WpfApp1
+namespace Bolygok
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static List<EllipsoidVisual3D> kijelolt = new();
+        public static Dictionary<EllipsoidVisual3D, Bolygo> osszes = new();
+        public static List<EllipsoidVisual3D> eddigBejart = new();
         public MainWindow()
         {
             InitializeComponent();
-            EllipsoidVisual3D utolsoMinimalis;
-            foreach (string item in File.ReadAllLines("gyongyok.txt").Skip(1).ToList())
+            foreach (string item in File.ReadAllLines("3D-G200-P865-ÉlSűrű.txt").Skip(1).ToList())
             {
                 string[] bontott = item.Split(';');
-                Gyongy gyongy = new(int.Parse(bontott[0]) * 3, int.Parse(bontott[1]) * 3, int.Parse(bontott[2]) * 3, int.Parse(bontott[3]));
-                EllipsoidVisual3D idk = new EllipsoidVisual3D();
-                idk.RadiusX = gyongy.E;
-                idk.RadiusY = gyongy.E;
-                idk.RadiusZ = gyongy.E;
-                idk.Center = new Point3D(gyongy.X, gyongy.Y, gyongy.Z);
-                idk.Fill = new SolidColorBrush(Colors.Red);
-                ter.Children.Add(idk);
-                
+                Bolygo bolygo = new(double.Parse(bontott[0]) * 3, double.Parse(bontott[1]) * 3, double.Parse(bontott[2]) * 3);
+                EllipsoidVisual3D terElem = new EllipsoidVisual3D();
+                terElem.RadiusX = 5;
+                terElem.RadiusY = 5;
+                terElem.RadiusZ = 5;
+                terElem.Center = new Point3D(bolygo.X, bolygo.Y, bolygo.Z);
+                terElem.Fill = new SolidColorBrush(Colors.Red);
+                ter.Children.Add(terElem);
+                osszes[terElem] = bolygo;
             }
 
+            
+        }
+
+        
+
+        private List<Bolygo> EltavolitBejart()
+        {
+            List<Bolygo> vissza = new();
+            foreach (var item in osszes)
+            {
+                if (!eddigBejart.Contains(item.Key))
+                {
+                    vissza.Add(item.Value);
+                }
+            }
+            return vissza;
+        }
+        
+        private List<EllipsoidVisual3D> UtvonalGenerator(Bolygo kezdo, Bolygo vegso)
+        {
+            List<Bolygo> bolygokLista = kezdo.VisszadobKoronBelul(EltavolitBejart());
+            Bolygo legkozelebbi = kezdo;
+            foreach (Bolygo bolygo in bolygokLista)
+            {
+                if (Bolygo.Tavolsag(vegso, legkozelebbi) < Bolygo.Tavolsag(vegso, kezdo))
+                {
+                    legkozelebbi = bolygo;
+                }
+            }
+            List<EllipsoidVisual3D> vissza = UtvonalGenerator(legkozelebbi, vegso);
+            
+            return vissza;
+        }
+
+        private void ter_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            HitTestResult htr = VisualTreeHelper.HitTest(ter, e.GetPosition(ter));
+            if (htr != null && htr.VisualHit is EllipsoidVisual3D) {
+                EllipsoidVisual3D clicked = (EllipsoidVisual3D)htr.VisualHit;
+                if (kijelolt.Count < 2)
+                {
+                    clicked.Fill = new SolidColorBrush(Colors.Blue);
+                    kijelolt.Add(clicked);
+                    return;
+                }
+                clicked.Fill = new SolidColorBrush(Colors.Red);
+                kijelolt.Remove(clicked);
+            }
+        }
+
+        private void btnRajzol_Click(object sender, RoutedEventArgs e)
+        {
+            if (kijelolt.Count == 2)
+            {
+                
+                LinesVisual3D vonalazas = new();
+                
+                foreach (var item in UtvonalGenerator(osszes[kijelolt[0]], osszes[kijelolt[1]]))
+                {
+
+                }
+                ter.Children.Add(vonalazas);
+
+            }
         }
     }
 }
